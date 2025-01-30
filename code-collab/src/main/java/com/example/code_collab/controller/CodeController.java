9169760242f6b12c47909516c5324279c1e8e3e2
GiharaNavindu <raviherath1961@gamil.com>
2025-01-30 11:32:15 +0530

    Controlller

--- code-collab/src/main/java/com/example/code_collab/controller/CodeController.java
+++ code-collab/src/main/java/com/example/code_collab/controller/CodeController.java
@@ -1,4 +1,30 @@
 package com.example.code_collab.controller;
 
+
+import com.example.code_collab.model.Room;
+import com.example.code_collab.service.CodeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/code")
 public class CodeController {
+
+    @Autowired
+    private CodeService codeService;
+
+    @PostMapping("/create")
+    public Room createRoom(@RequestParam String roomId) {
+        return codeService.createRoom(roomId);
+    }
+
+    @GetMapping("/get")
+    public Room getRoom(@RequestParam String roomId) {
+        return codeService.getRoom(roomId);
+    }
+
+    @PostMapping("/update")
+    public Room updateCode(@RequestParam String roomId, @RequestBody String code) {
+        return codeService.updateCode(roomId, code);
+    }
 }
