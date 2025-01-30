da00002fd97e215423a2cea36c6b8e2f7ba5c6a9
GiharaNavindu <raviherath1961@gamil.com>
2025-01-30 11:32:30 +0530

    model

--- code-collab/src/main/java/com/example/code_collab/model/Room.java
+++ code-collab/src/main/java/com/example/code_collab/model/Room.java
@@ -1,4 +1,40 @@
 package com.example.code_collab.model;
 
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class Room {
+    @Id
+    private String id;
+    private String roomId;
+    private String code;
+
+    // Getters and Setters
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setRoomId(String roomId) {
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
 }
